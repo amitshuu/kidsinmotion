44607b38b2e62321224e11cdc6880c162b702b1d
Amit <amitshuu@gmail.com>
2023-03-11 20:25:44 +0200

    Nav links

---
+++ src/utils/navLinks.ts
@@ -0,0 +1,24 @@
+import NavbarLogo from '../components/NavbarLogo';
+
+export const nav_links = [
+  {
+    title: 'גלרייה',
+    to: 'gallery',
+  },
+  {
+    title: 'פעילויות',
+    to: 'activities',
+  },
+  {
+    icon: NavbarLogo,
+    to: 'home',
+  },
+  {
+    title: 'אודות',
+    to: 'about-us',
+  },
+  {
+    title: 'בית',
+    to: 'home',
+  },
+];
