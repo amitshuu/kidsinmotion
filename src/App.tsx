b44765605fadd082519bb241535600df0f035d18
Amit <amitshuu@gmail.com>
2023-03-11 20:24:01 +0200

    Lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import AboutUs from './components/AboutUs';
 import LifeSkills from './components/LifeSkills';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import OurActivities from './components/OurActivities';
 import OurTeachers from './components/OurTeachers';
+const Gallery = lazy(() => import('./components/Gallery'));
 
 const App = () => {
   return (
     <main className='section-center'>
       <Navbar />
-
       <Hero />
       <AboutUs />
       <LifeSkills />
       <OurActivities />
       <OurTeachers />
+
+      <Gallery />
     </main>
   );
 };
