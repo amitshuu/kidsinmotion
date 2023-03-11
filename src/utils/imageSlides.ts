0c59ddcf08704b7795507a9f9baaea8809572690
Amit <amitshuu@gmail.com>
2023-03-11 20:25:53 +0200

    dummy images

---
+++ src/utils/imageSlides.ts
@@ -0,0 +1,15 @@
+import breakdance from '../assets/breakdance.jpg';
+import breakdancee from '../assets/breakdancee.jpg';
+import breakdanceee from '../assets/capoeira.jpg';
+
+export const slide_images = [
+  {
+    img: breakdance,
+  },
+  {
+    img: breakdancee,
+  },
+  {
+    img: breakdanceee,
+  },
+];
